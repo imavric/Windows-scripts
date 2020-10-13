525fa5658a516568e7bb6d2705b83c1c89dd76c1
imavric <72819232+imavric@users.noreply.github.com>
2020-10-13 20:04:14 +0200

    Update Clear_PrintSpool.bat

--- Clear_PrintSpool.bat
+++ Clear_PrintSpool.bat
@@ -1,1 +1,30 @@
+echo off
 
+echo.
+echo  :: Provera Administratorskih privilegija... (Checking For Administrator Elevation...)
+echo.
+timeout /t 1 /nobreak > NUL
+openfiles > NUL 2>&1
+if %errorlevel%==0 (
+        echo Privilegije OK. Nastavljam... (Elevation found! Proceeding...)
+) else (
+        echo  :: Niste pokrenuli skript kao Administrator. (You are NOT running as Administrator)
+        echo.
+        echo     Desni klik na ikonu pa odaberite ^'Run as Administrator^'. (Right-click and select ^'Run as Administrator^' and try again.)
+        echo     Pritisnite bilo koje dugme za kraj... (Press any key to exit...)
+        pause > NUL
+        exit
+)
+
+rem Clear print queue
+echo. 
+echo Početak čišćenja. (Clearing print queue.)
+echo.
+net stop spooler
+del %systemroot%\System32\spool\printers\* /Q /F /S
+net start spooler
+echo.
+echo Završeno. (Finished.)
+echo.
+
+timeout 5
