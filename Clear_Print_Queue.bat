7782a9f7b2420ae6b97cb6a88cb4b8072cecd9f0
imavric <72819232+imavric@users.noreply.github.com>
2020-10-17 18:32:53 +0200

    Update Clear_Print_Queue.bat

--- Clear_Print_Queue.bat
+++ Clear_Print_Queue.bat
@@ -10,11 +10,11 @@ echo.
 timeout /t 1 /nobreak > NUL
 openfiles > NUL 2>&1
 if %errorlevel%==0 (
-        echo Privilegije OK. Nastavljam... (Elevation found! Proceeding...)
+        echo Privilegije OK. Nastavljam...
 ) else (
-        echo  :: Niste pokrenuli skript kao Administrator. (You are NOT running as Administrator)
-        echo     Desni klik na ikonu pa odaberite ^'Run as Administrator^'. (Right-click and select ^'Run as Administrator^' and try again.)
-        echo     Pritisnite bilo koje dugme za kraj... (Press any key to exit...)
+        echo  :: Niste pokrenuli skript kao Administrator.
+        echo     Desni klik na ikonu pa odaberite ^'Run as Administrator^'.
+        echo     Pritisnite bilo koje dugme za kraj...
         echo.
         echo  :: You are NOT running as Administrator.
         echo     Right-click and select ^'Run as Administrator^' and try again.
